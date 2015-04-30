3a696c3d4f38c54a2aefd3b7099dedb882cc9760
MersalinJosh <m.mersalin@live.in>
2015-04-30 20:17:35 +0530

    Initial commit
    first commit with intilliJ initial setup

---
+++ src/main/java/com/socianser/Application.java
@@ -0,0 +1,12 @@
+package com.socianser;
+
+/**
+ * Created by Mersalin on 30-04-2015.
+ */
+public class Application {
+    public static void main(String args[]) {
+        System.out.println("Hello from Socianser!");
+    }
+}
+
+
